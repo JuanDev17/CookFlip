b45298aac8501e4afee35f596bee5affbfebf4df
Juan Aviles <juanaviles@Juans-MacBook-Air.local>
2026-09-02 01:51:00 -0400

    working on trending boards ui w/ mock data

--- src/app/components/board-card/board-card.ts
+++ src/app/components/board-card/board-card.ts
@@ -1,14 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BoardService } from './services/board-service';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { PopularBoard } from '../../api/types/board.type';
 
 @Component({
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   selector: 'app-board-card',
   styleUrl: './board-card.scss',
   templateUrl: './board-card.html',
-  [provider]
+  providers: [BoardService],
 })
 export class BoardCard {
 
+  // input property to receive 
+  @Input() board!: PopularBoard;
 
 }
